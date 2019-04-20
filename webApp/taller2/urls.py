6df8614720ec4ece1498a1338c9fcf13fbe382ba
Pedro Rozo <pedro.rozo@gmail.com>
2019-04-19 20:10:13 -0500

    quito url taller2 solo. El home pasa a #taller2

--- webApp/taller2/urls.py
+++ webApp/taller2/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    #path('', views.index, name='index'),
     path('presentacionDB/', views.presentacionDB, name='presentacionDB'),
     path('charts/', views.charts, name='charts'),
     path('hashtagWordCloud/', views.hashtagWordCloud, name='hashtagWordCloud'),
