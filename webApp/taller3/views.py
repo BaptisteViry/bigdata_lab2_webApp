dce07dce5f64c80585e92dd219c36be17c454bf5
Pedro Rozo <pedro.rozo@gmail.com>
2019-05-19 11:44:52 -0500

    guardar twitter stream en mongo

--- webApp/taller3/views.py
+++ webApp/taller3/views.py
@@ -10,6 +10,7 @@ from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
 import tweepy
+import threading
 
 
 from .forms import Question
@@ -21,6 +22,7 @@ client = MongoClient('bigdata-mongodb-04.virtual.uniandes.edu.co', 8087)
 #client = MongoClient('localhost', 27017)
 db = client.Grupo10_Taller3
 entities = db.music_questions_entities
+tweets=db.music_twitter
 
 
 
@@ -212,7 +214,13 @@ def artistSpotify(name):
 
  
 def strTwitter(request, searchText):
+    thread1 = threading.Thread(target = lanzarcreacion, args = (searchText,))
+    thread1.start()
+    #lanzarcreacion(searchText)
+    print ("se inició el hilo de recuperación de datos")
+    return render(request, 'taller3/streamTwitter.html')
 
+def lanzarcreacion(searchText):
     CONSUMER_KEY = "nqWae4EAcs4xSn9rVK4T0gZzS" 
     CONSUMER_SECRET = "ekuALYxFHFxMxR7yQitc8fCg0XcWuaI3flMaBdNgyKnZtcsGJ7"
     ACCESS_TOKEN = "4546199003-O19nOXfirpmgzMSuWJotbMNGYgAdHNpM7uyPqGz"
@@ -222,26 +230,25 @@ def strTwitter(request, searchText):
     auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 
     #searchText = " Michael  Jackson "
+    print ("se va a iniciar el listener..........")
 
     lisRead  =  listener()
     twitterStream = Stream(auth, lisRead, lang='en', tweet_mode='extended')
     twitterStream.filter(track=[searchText])
  
-    data = lisRead.datos
-    busqueda = searchText
-
-    print( "cantidad de datos ",str(len(data))) 
-
-    context={ 'data': data, "busqueda" : busqueda}    
-    return render(request, 'taller3/streamTwitter.html', context)
-
 class listener(StreamListener):
 
     limit = 5
     count = 0
     datos = []
 
+    
+    def __init__(self, lim=10):        
+        self.limit=lim
+
+
     def on_status(self, status):
+        print ("status...."+ status.text)
         self.count+=1
         return False if self.count == self.limit else True
 
@@ -249,6 +256,9 @@ class listener(StreamListener):
 
         print(data)
         self.datos.append(json.loads(data))    
+        print ("creando.......")
+        tweets.insert_one(json.loads(data))        
+        print (".......Dato creado: ")
         self.count+=1
         if self.count == self.limit:
             print("SALIDA POR LIMITE")
